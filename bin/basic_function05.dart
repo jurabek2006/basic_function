b446beeabb18100a47993e70716da1fc48198e17
jurabek2006 <yuldoshevjurabek@gmail.com>
2023-05-31 14:03:24 +0500

    s

--- bin/basic_function05.dart
+++ bin/basic_function05.dart
@@ -1,1 +1,9 @@
 // Create a function called `getFullName` that returns a `String`. The function should return your full name.
+  String getFullName() {
+  return "jurabek";
+  }
+
+void main() {
+    String a = getFullName();
+    print(a);
+}
